5e93715179eea443494063b6b7ab5802a16200c1
gauthami2019 <sgauthami68@gmail.com>
2022-07-28 09:57:28 +0530

    Updates added

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import ChooseAnOrphanage from "./screens/ChooseAnOrphanage";
+//import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView } from "react-native";
+import MyAccount from "./screens/MyAccount";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Login></Login>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MyAccount />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  root: {
+    backgroundColor: "#F9FBFC",
   },
 });
