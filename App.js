177258cddadd9edf03af42be0ffe998b7545bff5
ashnamariet <ashnamarietshaji2023@cs.ajce.in>
2022-07-28 10:30:27 +0530

    first commit

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ChooseAnOrphanage from './screens/ChooseAnOrphanage';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <ChooseAnOrphanage></ChooseAnOrphanage>
-      <StatusBar style="auto" />
+    <View>
+     
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
